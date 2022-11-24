4eb0613184d935fe78ede98720b9b5ef202a72e3
ErrolCord <errolcorderoc@gmail.com>
2022-11-24 01:36:45 -0600

    Porbando visual

--- Halltest.ino
+++ Halltest.ino
@@ -27,5 +27,5 @@ void loop()
   Serial.println(magField); 
   Serial.println("---------------------------------------");
   delay(1000);
-  Serial.print ("Probando para github");
+  Serial.print ("Probando para github y guía");
 }
