bb007c51c7b357806d6353289add90ecf26fab55
ErrolCord <errolcorderoc@gmail.com>
2022-11-24 02:21:05 -0600

    Probando push en visual

--- Halltest.ino
+++ Halltest.ino
@@ -29,5 +29,5 @@ void loop()
   Serial.println(magField); 
   Serial.println("---------------------------------------");
   delay(1000);
-  Serial.print ("Probando push");
+  Serial.print ("Probando push con visual 2");
 }
